e795ff7fbc94b8c13c4c3d6b044e39d7ca7f83ac
JeremyFontijne <jeremyfontijne@hotmail.com>
2019-06-13 14:57:59 +0200

    Add main activity for logins

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,99 @@
+package com.example.myapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+import com.twitter.sdk.android.core.*;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
+
+public class MainActivity extends Activity {
+
+    private TwitterLoginButton twitterLoginButton;
+    private PackageManager packageManager;
+
+    private String username;
+    private TwitterSession session;
+    private long userID;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Twitter.initialize(this);
+
+        setContentView(R.layout.activity_main);
+
+        //Check installed applications
+        packageManager = this.getPackageManager();
+        if (isPackageInstalled("com.linkedin.android", packageManager)) {
+
+        }
+
+        if (isPackageInstalled("com.twitter.android", packageManager)) {
+            twitterLoginButton = findViewById(R.id.twitter_login_button);
+            twitterLoginButton.setVisibility(View.VISIBLE);
+
+            //Handle Twitter login button
+            twitterLoginButton.setCallback(new Callback<TwitterSession>() {
+                @Override
+                public void success(Result<TwitterSession> result) {
+                    session = TwitterCore.getInstance().getSessionManager().getActiveSession();
+                    TwitterAuthToken authToken = session.getAuthToken();
+                    String token = authToken.token;
+                    String secret = authToken.secret;
+                    userID = result.data.getId();
+                    login(session);
+                }
+
+                @Override
+                public void failure(TwitterException exception) {
+                    exception.printStackTrace();
+                    Toast.makeText(MainActivity.this, "Login failed", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+        //Handle SenderActivity button click
+        ImageView img = findViewById(R.id.StartConnecting);
+        img.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SenderActivity.class);
+                intent.putExtra("UserID", userID);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void login(TwitterSession session) {
+        username = session.getUserName();
+
+        Toast.makeText(MainActivity.this, "Logged in to Twitter", Toast.LENGTH_LONG).show();
+        twitterLoginButton.setVisibility(View.INVISIBLE);
+    }
+
+    //Check if package is installed on device
+    private boolean isPackageInstalled(String packageName, PackageManager packageManager) {
+
+        boolean found = true;
+
+        try {
+            packageManager.getPackageInfo(packageName, 0);
+        } catch (PackageManager.NameNotFoundException e) {
+            found = false;
+        }
+
+        return found;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        twitterLoginButton.onActivityResult(requestCode, resultCode, data);
+    }
+
+}
--- app/src/main/java/com/example/myapplication/SenderActivity.java
+++ app/src/main/java/com/example/myapplication/SenderActivity.java
@@ -1,40 +1,24 @@
 package com.example.myapplication;
 
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.nfc.NfcAdapter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.*;
 
-import com.twitter.sdk.android.core.*;
-import com.twitter.sdk.android.core.identity.TwitterLoginButton;
-
 public class SenderActivity extends AppCompatActivity implements OutcomingNfcManager.NfcActivity {
-
-    private TextView tvOutcomingMessage;
-    private EditText etOutcomingMessage;
-    private Button btnSetOutcomingMessage;
 
     private NfcAdapter nfcAdapter;
     private OutcomingNfcManager outcomingNfccallback;
 
-    private PackageManager packageManager;
-
-    private TwitterLoginButton loginButton;
-
-    private String username;
-    private TwitterSession session;
     private long userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Twitter.initialize(this);
 
         setContentView(R.layout.activity_sender);
 
-        packageManager = this.getPackageManager();
         if (!isNfcSupported()) {
             Toast.makeText(this, "Nfc is not supported on this device", Toast.LENGTH_SHORT).show();
             finish();
@@ -43,59 +27,13 @@ public class SenderActivity extends AppCompatActivity implements OutcomingNfcMan
             Toast.makeText(this, "NFC disabled on this device. Turn on to proceed", Toast.LENGTH_SHORT).show();
         }
 
-        if (isPackageInstalled("com.linkedin.android", packageManager)) {
-            TextView textView = findViewById(R.id.linkedin);
-            textView.setText("LinkedIn is installed");
-        }
-
-        if (isPackageInstalled("com.twitter.android", packageManager)) {
-            TextView textView = findViewById(R.id.twitter);
-            textView.setText("Twitter is installed");
-        }
-
-        initViews();
+        Intent intent = getIntent();
+        userID = intent.getLongExtra("UserID", 0);
 
         // encapsulate sending logic in a separate class
         this.outcomingNfccallback = new OutcomingNfcManager(this);
         this.nfcAdapter.setOnNdefPushCompleteCallback(outcomingNfccallback, this);
         this.nfcAdapter.setNdefPushMessageCallback(outcomingNfccallback, this);
-
-        loginButton = (TwitterLoginButton) findViewById(R.id.login_button);
-        loginButton.setCallback(new Callback<TwitterSession>() {
-            @Override
-            public void success(Result<TwitterSession> result) {
-                session = TwitterCore.getInstance().getSessionManager().getActiveSession();
-                TwitterAuthToken authToken = session.getAuthToken();
-                String token = authToken.token;
-                String secret = authToken.secret;
-                userID = result.data.getId();
-                login(session);
-            }
-
-            @Override
-            public void failure(TwitterException exception) {
-                exception.printStackTrace();
-                Toast.makeText(SenderActivity.this, "Login failed", Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
-
-    private void login(TwitterSession session) {
-        username = session.getUserName();
-        TextView textView = findViewById(R.id.twitterLoggedIn);
-        textView.setText(username + " is now logged in on Twitter");
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        loginButton.onActivityResult(requestCode, resultCode, data);
-    }
-
-
-    private void initViews() {
-        this.tvOutcomingMessage = findViewById(R.id.linkedin);
     }
 
     @Override
@@ -120,19 +58,5 @@ public class SenderActivity extends AppCompatActivity implements OutcomingNfcMan
         // cause onNdefPushComplete is called from the Binder thread
         runOnUiThread(() ->
                 Toast.makeText(SenderActivity.this, R.string.message_beaming_complete, Toast.LENGTH_SHORT).show());
-    }
-
-    //Check if package is installed on device
-    private boolean isPackageInstalled(String packageName, PackageManager packageManager) {
-
-        boolean found = true;
-
-        try {
-            packageManager.getPackageInfo(packageName, 0);
-        } catch (PackageManager.NameNotFoundException e) {
-            found = false;
-        }
-
-        return found;
     }
 }
